1af3baf3d27966a8aa5d044fc8206cb7a7816ba7
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-07-24 19:04:56 +0300

    Initial code commit

---
+++ src/main/java/com/epam/reportportal/cucumber/RunningContext.java
@@ -0,0 +1,279 @@
+/*
+ * Copyright 2018 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.epam.reportportal.cucumber;
+
+import com.epam.ta.reportportal.ws.model.attribute.ItemAttributesRQ;
+import io.cucumber.core.gherkin.*;
+import io.cucumber.plugin.event.PickleStepTestStep;
+import io.cucumber.plugin.event.TestCase;
+import io.cucumber.plugin.event.TestSourceRead;
+import io.cucumber.plugin.event.TestStep;
+import io.reactivex.Maybe;
+
+import java.net.URI;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Running context that contains mostly manipulations with Gherkin objects.
+ * Keeps necessary information regarding current Feature, Scenario and Step
+ *
+ * @author Serhii Zharskyi
+ * @author Vitaliy Tsvihun
+ */
+class RunningContext {
+
+    private RunningContext() {
+        throw new AssertionError("No instances should exist for the class!");
+    }
+
+    static class FeatureContext {
+        private static Map<URI, TestSourceRead> pathToReadEventMap = new HashMap<>();
+        private URI currentFeatureUri;
+        private Maybe<String> currentFeatureId;
+        private Feature currentFeature;
+        private Set<ItemAttributesRQ> attributes;
+
+        FeatureContext() {
+            attributes = new HashSet<ItemAttributesRQ>();
+        }
+
+        static void addTestSourceReadEvent(URI uri, TestSourceRead event) {
+            pathToReadEventMap.put(uri, event);
+        }
+
+        ScenarioContext getScenarioContext(TestCase testCase) {
+            ScenarioDefinition scenario = getScenario(testCase);
+            ScenarioContext context = new ScenarioContext();
+            context.processTags(testCase.getTags());
+            context.processScenario(scenario);
+            context.setTestCase(testCase);
+            context.processBackground(getBackground());
+            context.processScenarioOutline(scenario);
+            return context;
+        }
+
+        FeatureContext processTestSourceReadEvent(TestCase testCase) {
+            TestSourceRead event = pathToReadEventMap.get(testCase.getUri());
+            currentFeature = getFeature(event.getSource());
+            currentFeatureUri = event.getUri();
+            attributes = Utils.extractAttributes(currentFeature.getTags());
+            return this;
+        }
+
+        Feature getFeature(String source) {
+            Parser<GherkinDocument> parser = new Parser<GherkinDocument>(new AstBuilder());
+            TokenMatcher matcher = new TokenMatcher();
+            GherkinDocument gherkinDocument;
+            try {
+                gherkinDocument = parser.parse(source, matcher);
+            } catch (ParserException e) {
+                // Ignore exceptions
+                return null;
+            }
+            return gherkinDocument.getFeature();
+        }
+
+        Background getBackground() {
+            ScenarioDefinition background = getFeature().getChildren().get(0);
+            return background instanceof Background ? (Background) background : null;
+        }
+
+        Feature getFeature() {
+            return currentFeature;
+        }
+
+        Set<ItemAttributesRQ> getAttributes() {
+            return attributes;
+        }
+
+        URI getUri() {
+            return currentFeatureUri;
+        }
+
+        Maybe<String> getFeatureId() {
+            return currentFeatureId;
+        }
+
+        void setFeatureId(Maybe<String> featureId) {
+            this.currentFeatureId = featureId;
+        }
+
+        @SuppressWarnings("unchecked")
+        <T extends ScenarioDefinition> T getScenario(TestCase testCase) {
+            List<ScenarioDefinition> featureScenarios = getFeature().getChildren();
+            for (ScenarioDefinition scenario : featureScenarios) {
+                if (scenario instanceof Background) {
+                    continue;
+                }
+                if (testCase.getLine() == scenario.getLocation().getLine() && testCase.getName().equals(scenario.getName())) {
+                    return (T) scenario;
+                } else {
+                    if (scenario instanceof ScenarioOutline) {
+                        for (Examples example : ((ScenarioOutline) scenario).getExamples()) {
+                            for (TableRow tableRow : example.getTableBody()) {
+                                if (tableRow.getLocation().getLine() == testCase.getLine()) {
+                                    return (T) scenario;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            throw new IllegalStateException("Scenario can't be null!");
+        }
+    }
+
+    static class ScenarioContext {
+        private static Map<ScenarioDefinition, List<Integer>> scenarioOutlineMap = new ConcurrentHashMap<>();
+        private Maybe<String> id = null;
+        private Background background;
+        private ScenarioDefinition scenario;
+        private Queue<Step> backgroundSteps;
+        private Map<Integer, Step> scenarioLocationMap;
+        private Set<ItemAttributesRQ> attributes;
+        private TestCase testCase;
+        private boolean hasBackground = false;
+        private String scenarioDesignation;
+        private String outlineIteration;
+
+        ScenarioContext() {
+            backgroundSteps = new ArrayDeque<Step>();
+            scenarioLocationMap = new HashMap<Integer, Step>();
+            attributes = new HashSet<ItemAttributesRQ>();
+        }
+
+        void processScenario(ScenarioDefinition scenario) {
+            this.scenario = scenario;
+            for (Step step : scenario.getSteps()) {
+                scenarioLocationMap.put(step.getLocation().getLine(), step);
+            }
+        }
+
+        void processBackground(Background background) {
+            if (background != null) {
+                this.background = background;
+                hasBackground = true;
+                backgroundSteps.addAll(background.getSteps());
+                mapBackgroundSteps(background);
+            }
+        }
+
+        public Set<ItemAttributesRQ> getAttributes() {
+            return attributes;
+        }
+
+        /**
+         * Takes the serial number of scenario outline and links it to the executing scenario
+         **/
+        void processScenarioOutline(ScenarioDefinition scenarioOutline) {
+            if (isScenarioOutline(scenarioOutline)) {
+                scenarioOutlineMap.computeIfAbsent(
+                        scenarioOutline,
+                        k -> ((ScenarioOutline) scenarioOutline).getExamples()
+                                .stream()
+                                .flatMap(e -> e.getTableBody().stream())
+                                .map(r -> r.getLocation().getLine())
+                                .collect(Collectors.toList())
+                );
+                int iterationIdx = IntStream.range(0, scenarioOutlineMap.get(scenarioOutline).size())
+                        .filter(i -> getLine() == scenarioOutlineMap.get(scenarioOutline).get(i))
+                        .findFirst()
+                        .orElseThrow(() -> new IllegalStateException(String.format(
+                                "No outline iteration number found for scenario %s",
+                                scenarioDesignation
+                        )));
+                outlineIteration = String.format("[%d]", iterationIdx + 1);
+            }
+        }
+
+        void processTags(List<String> pickleTags) {
+            attributes = Utils.extractAttributes(pickleTags);
+        }
+
+        void mapBackgroundSteps(Background background) {
+            for (Step step : background.getSteps()) {
+                scenarioLocationMap.put(step.getLocation().getLine(), step);
+            }
+        }
+
+        String getName() {
+            return scenario.getName();
+        }
+
+        String getKeyword() {
+            return scenario.getKeyWord();
+        }
+
+        int getLine() {
+            return isScenarioOutline(scenario) ? testCase.getLine() : scenario.getLocation().getLine();
+        }
+
+        String getStepPrefix() {
+            return hasBackground() && withBackground() ? background.getKeyword().toUpperCase() + AbstractReporter.COLON_INFIX : "";
+        }
+
+        Step getStep(TestStep testStep) {
+            PickleStepTestStep pickleStepTestStep = (PickleStepTestStep) testStep;
+            Step step = scenarioLocationMap.get(pickleStepTestStep.getStepLine());
+            if (step != null) {
+                return step;
+            }
+            throw new IllegalStateException(String.format("Trying to get step for unknown line in feature. " + "Scenario: %s, line: %s",
+                    scenario.getName(),
+                    getLine()
+            ));
+        }
+
+        Maybe<String> getId() {
+            return id;
+        }
+
+        void setId(Maybe<String> newId) {
+            if (id != null) {
+                throw new IllegalStateException("Attempting re-set scenario ID for unfinished scenario: " + getName());
+            }
+            id = newId;
+        }
+
+        void setTestCase(TestCase testCase) {
+            this.testCase = testCase;
+            scenarioDesignation = testCase.getScenarioDesignation();
+        }
+
+        void nextBackgroundStep() {
+            backgroundSteps.poll();
+        }
+
+        boolean isScenarioOutline(ScenarioDefinition scenario) {
+            return scenario instanceof ScenarioOutline;
+        }
+
+        boolean withBackground() {
+            return !backgroundSteps.isEmpty();
+        }
+
+        boolean hasBackground() {
+            return hasBackground && background != null;
+        }
+
+        String getOutlineIteration() {
+            return outlineIteration;
+        }
+    }
+}
---
+++ src/main/java/com/epam/reportportal/cucumber/Utils.java
@@ -0,0 +1,380 @@
+/*
+ * Copyright 2018 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.epam.reportportal.cucumber;
+
+import com.epam.reportportal.annotations.TestCaseId;
+import com.epam.reportportal.annotations.attribute.Attributes;
+import com.epam.reportportal.listeners.ItemStatus;
+import com.epam.reportportal.service.Launch;
+import com.epam.reportportal.service.ReportPortal;
+import com.epam.reportportal.service.item.TestCaseIdEntry;
+import com.epam.reportportal.utils.AttributeParser;
+import com.epam.reportportal.utils.TestCaseIdUtils;
+import com.epam.ta.reportportal.ws.model.FinishTestItemRQ;
+import com.epam.ta.reportportal.ws.model.ParameterResource;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
+import com.epam.ta.reportportal.ws.model.attribute.ItemAttributesRQ;
+
+import io.cucumber.plugin.event.PickleStepTestStep;
+import io.cucumber.plugin.event.TestStep;
+import io.cucumber.plugin.event.Status;
+import io.reactivex.Maybe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rp.com.google.common.collect.ImmutableMap;
+import rp.com.google.common.collect.Lists;
+
+import javax.annotation.Nullable;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Optional.ofNullable;
+
+public class Utils {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Utils.class);
+	private static final String TABLE_INDENT = "          ";
+	private static final String TABLE_SEPARATOR = "|";
+	private static final String DOCSTRING_DECORATOR = "\n\"\"\"\n";
+	private static final String PASSED = "passed";
+	private static final String SKIPPED = "skipped";
+	private static final String INFO = "INFO";
+	private static final String WARN = "WARN";
+	private static final String ERROR = "ERROR";
+	private static final String EMPTY = "";
+	private static final String ONE_SPACE = " ";
+	private static final String HOOK_ = "Hook: ";
+	private static final String NEW_LINE = "\r\n";
+
+	private static final String DEFINITION_MATCH_FIELD_NAME = "definitionMatch";
+	private static final String STEP_DEFINITION_FIELD_NAME = "stepDefinition";
+	private static final String GET_LOCATION_METHOD_NAME = "getLocation";
+	private static final String METHOD_OPENING_BRACKET = "(";
+	private static final String METHOD_FIELD_NAME = "method";
+	private static final String PARAMETER_REGEX = "<[^<>]+>";
+
+	private Utils() {
+		throw new AssertionError("No instances should exist for the class!");
+	}
+
+	//@formatter:off
+    private static final Map<Status, ItemStatus> STATUS_MAPPING = ImmutableMap.<Status, ItemStatus>builder()
+            .put(Status.PASSED, ItemStatus.PASSED)
+            .put(Status.FAILED, ItemStatus.FAILED)
+            .put(Status.SKIPPED, ItemStatus.SKIPPED)
+            .put(Status.PENDING, ItemStatus.SKIPPED)
+            .put(Status.AMBIGUOUS, ItemStatus.SKIPPED)
+            .put(Status.UNDEFINED, ItemStatus.SKIPPED)
+            .put(Status.UNUSED, ItemStatus.SKIPPED).build();
+    //@formatter:on
+
+	static void finishFeature(Launch rp, Maybe<String> itemId, Date dateTime) {
+		if (itemId == null) {
+			LOGGER.error("BUG: Trying to finish unspecified test item.");
+			return;
+		}
+		FinishTestItemRQ rq = new FinishTestItemRQ();
+		rq.setEndTime(dateTime);
+		rp.finishTestItem(itemId, rq);
+	}
+
+	static void finishTestItem(Launch rp, Maybe<String> itemId) {
+		finishTestItem(rp, itemId, null);
+	}
+
+	static Date finishTestItem(Launch rp, Maybe<String> itemId, Status status) {
+		if (itemId == null) {
+			LOGGER.error("BUG: Trying to finish unspecified test item.");
+			return null;
+		}
+		FinishTestItemRQ rq = new FinishTestItemRQ();
+		Date endTime = Calendar.getInstance().getTime();
+		rq.setEndTime(endTime);
+		rq.setStatus(mapItemStatus(status));
+		rp.finishTestItem(itemId, rq);
+		return endTime;
+	}
+
+	static Maybe<String> startNonLeafNode(Launch rp, Maybe<String> rootItemId, String name, String description,
+			Set<ItemAttributesRQ> attributes, String type) {
+		StartTestItemRQ rq = new StartTestItemRQ();
+		rq.setDescription(description);
+		rq.setName(name);
+		rq.setAttributes(attributes);
+		rq.setStartTime(Calendar.getInstance().getTime());
+		rq.setType(type);
+		return rp.startTestItem(rootItemId, rq);
+	}
+
+	static void sendLog(final String message, final String level) {
+		ReportPortal.emitLog(message, level, Calendar.getInstance().getTime());
+	}
+
+	/**
+	 * Transform tags from Cucumber to RP format
+	 *
+	 * @param tags - Cucumber tags
+	 * @return set of tags
+	 */
+	public static Set<ItemAttributesRQ> extractAttributes(List<String> tags) {
+		return tags.stream().map(s->new ItemAttributesRQ(null, s)).collect(Collectors.toSet());
+	}
+
+	/**
+	 * Map Cucumber statuses to RP log levels
+	 *
+	 * @param cukesStatus - Cucumber status
+	 * @return regular log level
+	 */
+	static String mapLevel(String cukesStatus) {
+		String mapped;
+		if (cukesStatus.equalsIgnoreCase(PASSED)) {
+			mapped = INFO;
+		} else if (cukesStatus.equalsIgnoreCase(SKIPPED)) {
+			mapped = WARN;
+		} else {
+			mapped = ERROR;
+		}
+		return mapped;
+	}
+
+    /**
+     * Map Cucumber statuses to RP item statuses
+     *
+     * @param status - Cucumber status
+     * @return RP test item status and null if status is null
+     */
+    static String mapItemStatus(Status status) {
+        if (status == null) {
+            return null;
+        } else {
+            if (STATUS_MAPPING.get(status) == null) {
+                LOGGER.error(String.format("Unable to find direct mapping between Cucumber and ReportPortal for TestItem with status: '%s'.", status));
+                return ItemStatus.SKIPPED.name();
+            }
+            return STATUS_MAPPING.get(status).name();
+        }
+    }
+
+	/**
+	 * Generate name representation
+	 *
+	 * @param prefix   - substring to be prepended at the beginning (optional)
+	 * @param infix    - substring to be inserted between keyword and name
+	 * @param argument - main text to process
+	 * @param suffix   - substring to be appended at the end (optional)
+	 * @return transformed string
+	 */
+	//TODO: pass Node as argument, not test event
+	static String buildNodeName(String prefix, String infix, String argument, String suffix) {
+		return buildName(prefix, infix, argument, suffix);
+	}
+
+	private static String buildName(String prefix, String infix, String argument, String suffix) {
+		return (prefix == null ? EMPTY : prefix) + infix + argument + (suffix == null ? EMPTY : suffix);
+	}
+
+	/**
+	 * Generate multiline argument (DataTable or DocString) representation
+	 *
+	 * @param step - Cucumber step object
+	 * @return - transformed multiline argument (or empty string if there is
+	 * none)
+	 */
+	static String buildMultilineArgument(TestStep step) {
+		List<PickleRow> table = null;
+		String dockString = EMPTY;
+		StringBuilder marg = new StringBuilder();
+		PickleStepTestStep pickleStep = (PickleStepTestStep) step;
+		if (!pickleStep.getStepArgument().isEmpty()) {
+			Argument argument = pickleStep.getStepArgument().get(0);
+			if (argument instanceof PickleString) {
+				dockString = ((PickleString) argument).getContent();
+			} else if (argument instanceof PickleTable) {
+				table = ((PickleTable) argument).getRows();
+			}
+		}
+		if (table != null) {
+			marg.append(NEW_LINE);
+			for (PickleRow row : table) {
+				marg.append(TABLE_INDENT).append(TABLE_SEPARATOR);
+				for (PickleCell cell : row.getCells()) {
+					marg.append(ONE_SPACE).append(cell.getValue()).append(ONE_SPACE).append(TABLE_SEPARATOR);
+				}
+				marg.append(NEW_LINE);
+			}
+		}
+
+		if (!dockString.isEmpty()) {
+			marg.append(DOCSTRING_DECORATOR).append(dockString).append(DOCSTRING_DECORATOR);
+		}
+		return marg.toString();
+	}
+
+	static String getStepName(TestStep step) {
+		return step instanceof HookTestStep ?
+				HOOK_ + ((HookTestStep) step).getHookType().toString() :
+				((PickleStepTestStep) step).getPickleStep().getText();
+	}
+
+	@Nullable
+	public static Set<ItemAttributesRQ> getAttributes(TestStep testStep) {
+		Field definitionMatchField = getDefinitionMatchField(testStep);
+		if (definitionMatchField != null) {
+			try {
+				Method method = retrieveMethod(definitionMatchField, testStep);
+				Attributes attributesAnnotation = method.getAnnotation(Attributes.class);
+				if (attributesAnnotation != null) {
+					return AttributeParser.retrieveAttributes(attributesAnnotation);
+				}
+			} catch (NoSuchFieldException | IllegalAccessException e) {
+				return null;
+			}
+		}
+		return null;
+	}
+
+	@Nullable
+	public static String getCodeRef(TestStep testStep) {
+
+		Field definitionMatchField = getDefinitionMatchField(testStep);
+
+		if (definitionMatchField != null) {
+
+			try {
+				StepDefinitionMatch stepDefinitionMatch = (StepDefinitionMatch) definitionMatchField.get(testStep);
+				Field stepDefinitionField = stepDefinitionMatch.getClass().getDeclaredField(STEP_DEFINITION_FIELD_NAME);
+				stepDefinitionField.setAccessible(true);
+				Object javaStepDefinition = stepDefinitionField.get(stepDefinitionMatch);
+				Method getLocationMethod = javaStepDefinition.getClass().getDeclaredMethod(GET_LOCATION_METHOD_NAME, boolean.class);
+				getLocationMethod.setAccessible(true);
+				String fullCodeRef = String.valueOf(getLocationMethod.invoke(javaStepDefinition, true));
+				return fullCodeRef != null ? fullCodeRef.substring(0, fullCodeRef.indexOf(METHOD_OPENING_BRACKET)) : null;
+			} catch (NoSuchFieldException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+				return null;
+			}
+
+		} else {
+			return null;
+		}
+
+	}
+
+	@Nullable
+	public static TestCaseIdEntry getTestCaseId(TestStep testStep, String codeRef) {
+		Field definitionMatchField = getDefinitionMatchField(testStep);
+		if (definitionMatchField != null) {
+			try {
+				Method method = retrieveMethod(definitionMatchField, testStep);
+				TestCaseId testCaseIdAnnotation = method.getAnnotation(TestCaseId.class);
+				return ofNullable(testCaseIdAnnotation).flatMap(annotation -> ofNullable(getTestCaseId(testCaseIdAnnotation,
+						method,
+						((PickleStepTestStep) testStep).getDefinitionArgument()
+				))).orElseGet(() -> getTestCaseId(codeRef, ((PickleStepTestStep) testStep).getDefinitionArgument()));
+			} catch (NoSuchFieldException | IllegalAccessException e) {
+				return getTestCaseId(codeRef, ((PickleStepTestStep) testStep).getDefinitionArgument());
+			}
+		} else {
+			return getTestCaseId(codeRef, ((PickleStepTestStep) testStep).getDefinitionArgument());
+		}
+	}
+
+	static List<ParameterResource> getParameters(List<cucumber.api.Argument> arguments, String text) {
+		List<ParameterResource> parameters = Lists.newArrayList();
+		ArrayList<String> parameterNames = Lists.newArrayList();
+		Matcher matcher = Pattern.compile(PARAMETER_REGEX).matcher(text);
+		while (matcher.find()) {
+			parameterNames.add(text.substring(matcher.start() + 1, matcher.end() - 1));
+		}
+		IntStream.range(0, parameterNames.size()).forEach(index -> {
+			String parameterName = parameterNames.get(index);
+			if (index < arguments.size()) {
+				String parameterValue = arguments.get(index).getValue();
+				ParameterResource parameterResource = new ParameterResource();
+				parameterResource.setKey(parameterName);
+				parameterResource.setValue(parameterValue);
+				parameters.add(parameterResource);
+			}
+		});
+		return parameters;
+	}
+
+	private static Method retrieveMethod(Field definitionMatchField, TestStep testStep)
+			throws IllegalAccessException, NoSuchFieldException {
+		StepDefinitionMatch stepDefinitionMatch = (StepDefinitionMatch) definitionMatchField.get(testStep);
+		Field stepDefinitionField = stepDefinitionMatch.getClass().getDeclaredField(STEP_DEFINITION_FIELD_NAME);
+		stepDefinitionField.setAccessible(true);
+		Object javaStepDefinition = stepDefinitionField.get(stepDefinitionMatch);
+		Field methodField = javaStepDefinition.getClass().getDeclaredField(METHOD_FIELD_NAME);
+		methodField.setAccessible(true);
+		return (Method) methodField.get(javaStepDefinition);
+	}
+
+	@Nullable
+	private static TestCaseIdEntry getTestCaseId(TestCaseId testCaseId, Method method, List<cucumber.api.Argument> arguments) {
+		if (testCaseId.parametrized()) {
+			List<String> values = new ArrayList<String>(arguments.size());
+			for (cucumber.api.Argument argument : arguments) {
+				values.add(argument.getValue());
+			}
+			return TestCaseIdUtils.getParameterizedTestCaseId(method, values.toArray());
+		} else {
+			return new TestCaseIdEntry(testCaseId.value());
+		}
+	}
+
+	private static TestCaseIdEntry getTestCaseId(String codeRef, List<cucumber.api.Argument> arguments) {
+		return ofNullable(arguments).filter(args -> !args.isEmpty())
+				.map(args -> new TestCaseIdEntry(codeRef + TRANSFORM_PARAMETERS.apply(args)))
+				.orElseGet(() -> new TestCaseIdEntry(codeRef));
+	}
+
+	private static final Function<List<cucumber.api.Argument>, String> TRANSFORM_PARAMETERS = it -> it.stream()
+			.map(cucumber.api.Argument::getValue)
+			.collect(Collectors.joining(",", "[", "]"));
+
+	private static Field getDefinitionMatchField(TestStep testStep) {
+
+		Class<?> clazz = testStep.getClass();
+
+		try {
+			return clazz.getField(DEFINITION_MATCH_FIELD_NAME);
+		} catch (NoSuchFieldException e) {
+			do {
+				try {
+					Field definitionMatchField = clazz.getDeclaredField(DEFINITION_MATCH_FIELD_NAME);
+					definitionMatchField.setAccessible(true);
+					return definitionMatchField;
+				} catch (NoSuchFieldException ignore) {
+				}
+
+				clazz = clazz.getSuperclass();
+			} while (clazz != null);
+
+			return null;
+		}
+	}
+}
