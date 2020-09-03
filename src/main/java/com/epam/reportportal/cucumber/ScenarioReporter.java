23b54598b4056c89878da412c1327ee7c82ab0cc
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-09-03 18:23:18 +0300

    Manually-reported nested steps now correctly fail all parents

--- src/main/java/com/epam/reportportal/cucumber/ScenarioReporter.java
+++ src/main/java/com/epam/reportportal/cucumber/ScenarioReporter.java
@@ -73,9 +73,9 @@ public class ScenarioReporter extends AbstractReporter {
 		reportResult(result, null);
 		RunningContext.ScenarioContext context = getCurrentScenarioContext();
 		Launch myLaunch = launch.get();
+		myLaunch.getStepReporter().finishPreviousStep();
 		Utils.finishTestItem(myLaunch, context.getCurrentStepId(), result.getStatus());
 		context.setCurrentStepId(null);
-		myLaunch.getStepReporter().finishPreviousStep();
 	}
 
 	@Override
@@ -96,9 +96,9 @@ public class ScenarioReporter extends AbstractReporter {
 	protected void afterHooks(Boolean isBefore) {
 		RunningContext.ScenarioContext context = getCurrentScenarioContext();
 		Launch myLaunch = launch.get();
+		myLaunch.getStepReporter().finishPreviousStep();
 		Utils.finishTestItem(myLaunch, context.getHookStepId(), context.getHookStatus());
 		context.setHookStepId(null);
-		myLaunch.getStepReporter().finishPreviousStep();
 	}
 
 	@Override
