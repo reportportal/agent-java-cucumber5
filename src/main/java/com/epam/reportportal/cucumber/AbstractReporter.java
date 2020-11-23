a04b2983097518ef6a5d02b4109b98de6db4ceb3
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-11-23 19:05:00 +0300

    Fixed 'CHILD_START_TIME_EARLIER_THAN_PARENT' Exception in some cases

--- src/main/java/com/epam/reportportal/cucumber/AbstractReporter.java
+++ src/main/java/com/epam/reportportal/cucumber/AbstractReporter.java
@@ -85,7 +85,6 @@ public abstract class AbstractReporter implements ConcurrentEventListener {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractReporter.class);
 	private static final URI WORKING_DIRECTORY = new File(System.getProperty("user.dir")).toURI();
 	private static final String AGENT_PROPERTIES_FILE = "agent.properties";
-	private static final int DEFAULT_CAPACITY = 16;
 	private static final String HOOK_ = "Hook: ";
 	private static final String GET_LOCATION_METHOD_NAME = "getLocation";
 	private static final String METHOD_OPENING_BRACKET = "(";
@@ -184,7 +183,7 @@ public abstract class AbstractReporter implements ConcurrentEventListener {
 
 	private void addToTree(RunningContext.FeatureContext featureContext, RunningContext.ScenarioContext scenarioContext) {
 		retrieveLeaf(featureContext.getUri(), ITEM_TREE).ifPresent(suiteLeaf -> suiteLeaf.getChildItems()
-				.put(createKey(scenarioContext.getLine()), TestItemTree.createTestItemLeaf(scenarioContext.getId(), DEFAULT_CAPACITY)));
+				.put(createKey(scenarioContext.getLine()), TestItemTree.createTestItemLeaf(scenarioContext.getId())));
 	}
 
 	/**
@@ -540,8 +539,8 @@ public abstract class AbstractReporter implements ConcurrentEventListener {
 	}
 
 	private RunningContext.FeatureContext startFeatureContext(RunningContext.FeatureContext context) {
-		StartTestItemRQ rq = buildStartFeatureRequest(context.getFeature(), context.getUri());
 		Optional<Maybe<String>> root = getRootItemId();
+		StartTestItemRQ rq = buildStartFeatureRequest(context.getFeature(), context.getUri());
 		context.setFeatureId(root.map(r -> launch.get().startTestItem(r, rq)).orElseGet(() -> launch.get().startTestItem(rq)));
 		return context;
 	}
@@ -600,7 +599,7 @@ public abstract class AbstractReporter implements ConcurrentEventListener {
 
 	private void addToTree(RunningContext.FeatureContext context) {
 		ITEM_TREE.getTestItems()
-				.put(createKey(context.getUri()), TestItemTree.createTestItemLeaf(context.getFeatureId(), DEFAULT_CAPACITY));
+				.put(createKey(context.getUri()), TestItemTree.createTestItemLeaf(context.getFeatureId()));
 	}
 
 	protected void handleStartOfTestCase(TestCaseStarted event) {
@@ -656,7 +655,7 @@ public abstract class AbstractReporter implements ConcurrentEventListener {
 		retrieveLeaf(scenarioContext.getFeatureUri(),
 				scenarioContext.getLine(),
 				ITEM_TREE
-		).ifPresent(scenarioLeaf -> scenarioLeaf.getChildItems().put(createKey(text), TestItemTree.createTestItemLeaf(stepId, 0)));
+		).ifPresent(scenarioLeaf -> scenarioLeaf.getChildItems().put(createKey(text), TestItemTree.createTestItemLeaf(stepId)));
 	}
 
 	protected void removeFromTree(RunningContext.ScenarioContext scenarioContext, String text) {
